b361983345cbac960dccc58b01ef72e44aab1966
robelix <roland@robelix.com>
2012-08-23 00:40:06 +0200

    device strings changed in ttymidi - if they are not the same for in and out mixxx thinks these are diffrent devices

--- ttymidi/src/ttymidi.c
+++ ttymidi/src/ttymidi.c
@@ -162,14 +162,14 @@ int open_seq(snd_seq_t** seq)
 
 	snd_seq_set_client_name(*seq, arguments.name);
 
-	if ((port_out_id = snd_seq_create_simple_port(*seq, "MIDI out",
+	if ((port_out_id = snd_seq_create_simple_port(*seq, "ttyMIDI",
 					SND_SEQ_PORT_CAP_READ|SND_SEQ_PORT_CAP_SUBS_READ,
 					SND_SEQ_PORT_TYPE_APPLICATION)) < 0) 
 	{
 		fprintf(stderr, "Error creating sequencer port.\n");
 	}
 
-	if ((port_in_id = snd_seq_create_simple_port(*seq, "MIDI in",
+	if ((port_in_id = snd_seq_create_simple_port(*seq, "ttyMIDI",
 					SND_SEQ_PORT_CAP_WRITE|SND_SEQ_PORT_CAP_SUBS_WRITE,
 					SND_SEQ_PORT_TYPE_APPLICATION)) < 0) 
 	{
